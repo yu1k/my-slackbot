fa3c5a3272984c61293f3fa4a193aca0d871af02
yu1k <48705423+yu1k@users.noreply.github.com>
2020-06-23 23:51:08 +0900

    add: timeTest()

---
+++ app.js
@@ -0,0 +1,17 @@
+"use strict"
+
+const cron = require("node-cron");
+
+//Node cronのテスト用
+function timeTest(){
+    //秒 分 時　日 月　曜日
+    cron.schedule("50 49 23 23 June *", () => {
+        console.log("hoge");
+    });
+}
+
+function main(){
+    timeTest();
+}
+
+main();
