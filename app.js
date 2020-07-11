0b482c9039ade119bbe27d024711bfb562b9f9a9
yu1k <48705423+yu1k@users.noreply.github.com>
2020-07-11 18:48:42 +0900

    add : help機能を実装

--- app.js
+++ app.js
@@ -36,11 +36,29 @@ module.exports = (robot) => {
         });
     }
 
+    function help(){
+        //受け取ったメッセージ
+        robot.hear(/botHelp>/i, response => {
+            //!helpなど
+            if(!("botHelp>")){
+                return;
+            }
+            response.send(
+                "このbotは の生活の助けをしてくれるbotです。" + "\n",
+                " 以外の人にはあまり役に立たないかもしれないので使わないことを推奨します。" + "\n",
+                "地名+の天気予報を教えて 、というとその地名の天気予報を教えてくれます。" + "\n",
+                "また、朝9時,お昼12時30分,午後4時,午後10時に時間を教えてくれます。"
+            );
+            //console.log("使い方");
+        });
+    }
+
     //関数をmain関数にまとめる
     function main(){
         timeTest();
         postNotice();
         weatherForecastPost();
+        help();
     }
 
     main();
