60185ff214908dd43664973a29ab42facbe5ace5
yu1k <48705423+yu1k@users.noreply.github.com>
2020-07-11 18:10:18 +0900

    Update : Hubotのmoduleを呼び出す書きかたに変更

--- app.js
+++ app.js
@@ -4,23 +4,26 @@ const cron = require("node-cron");
 
 const apiToken = "xxxxxxxxxxx";
 
-//Node cronのテスト用
-function timeTest(){
-    //秒 分 時　日 月　曜日
-    cron.schedule("50 49 23 23 June *", () => {
-        console.log("hoge");
-    });
-}
+module.exports = (robot) => {
+    //Node cronのテスト用
+    function timeTest(){
+        //秒 分 時　日 月　曜日
+        cron.schedule("50 49 23 23 June *", () => {
+            console.log("hoge");
+        });
+    }
 
-function postNotice(){
-    cron.schedule("* * * * * *", () => {
-        console.log("hoge");
-    });
-}
+    function postNotice(){
+        cron.schedule("* * * * * *", () => {
+            console.log("hoge");
+        });
+    }
 
-function main(){
-    timeTest();
-    postNotice();
-}
+    //関数をmain関数にまとめる
+    function main(){
+        timeTest();
+        postNotice();
+    }
 
-main();
+    main();
+};
