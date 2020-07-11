ebcd7d32da23ffa8c021be73206d154067d0f396
yu1k <48705423+yu1k@users.noreply.github.com>
2020-07-11 18:25:30 +0900

    add : 天気予報を拾ってきて出力してくれる機能を実装

--- app.js
+++ app.js
@@ -20,10 +20,27 @@ module.exports = (robot) => {
         });
     }
 
+    //天気予報を教えてくれる機能
+    //まだ動くかテストしてないです
+    var location = ""; //受け取った文字を入れておく変数
+
+    function weatherForecastPost(){
+        robot.hear(/の天気予報を教えて/, msg => { //メッセージ読み込みの形式 : osakaの天気予報を教えて //受け取った文字列
+        location = msg.massage;
+        if(!location){
+            return;
+        }
+        var result = location.slice(0, -9); //"の天気予報を教えて"という部分を削除
+        msg.send(`https://wttr.in/` + `<${result}>`);
+        //console.log("https://wttr.in/" + location);
+        });
+    }
+
     //関数をmain関数にまとめる
     function main(){
         timeTest();
         postNotice();
+        weatherForecastPost();
     }
 
     main();
